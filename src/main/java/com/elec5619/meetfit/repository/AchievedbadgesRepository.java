f539494813c2f7db6eb64f66ed04cedf3b5f6759
Ahmed <asha2285@uni.sydney.edu.au>
2016-10-25 01:08:47 +1100

    sorted achievedbadges

--- src/main/java/com/elec5619/meetfit/repository/AchievedbadgesRepository.java
+++ src/main/java/com/elec5619/meetfit/repository/AchievedbadgesRepository.java
@@ -12,4 +12,7 @@ import java.util.List;
 @SuppressWarnings("unused")
 public interface AchievedbadgesRepository extends JpaRepository<Achievedbadges,Long> {
 
+    List<Achievedbadges> findAllByOrderByPointsAsc();
+
+    List<Achievedbadges> findAllByOrderByPointsDesc();
 }
