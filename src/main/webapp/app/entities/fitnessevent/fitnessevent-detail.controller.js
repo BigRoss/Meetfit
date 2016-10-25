98312deb9b6d4b375b0bc55d5beacd7c9198b0f7
Jeff-Laptop\Jeff <jlie6227@uni.sydney.edu.au>
2016-10-25 19:12:45 +1100

    Allow users to join events from the events detail page

--- src/main/webapp/app/entities/fitnessevent/fitnessevent-detail.controller.js
+++ src/main/webapp/app/entities/fitnessevent/fitnessevent-detail.controller.js
@@ -5,17 +5,36 @@
         .module('meetFitApp')
         .controller('FitnesseventDetailController', FitnesseventDetailController);
 
-    FitnesseventDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Fitnessevent', 'User'];
+    FitnesseventDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Fitnessevent', 'Attendingevent', 'User'];
 
-    function FitnesseventDetailController($scope, $rootScope, $stateParams, previousState, entity, Fitnessevent, User) {
+    function FitnesseventDetailController($scope, $rootScope, $stateParams, previousState, entity, Fitnessevent, Attendingevent, User) {
         var vm = this;
 
         vm.fitnessevent = entity;
         vm.previousState = previousState.name;
+        vm.confirmJoin = confirmJoin;
+
+        function confirmJoin (id)
+        {
+            Attendingevent.attend(vm.fitnessevent, goodAlert, badAlert);
+            $uibModalInstance.close(true);
+        }
+
+        function goodAlert(result)
+        {
+            //alert('yay: ' + result);
+            console.log(result);
+        }
+
+        function badAlert(result)
+        {
+            //alert('bad: ' +result);
+        }
 
         var unsubscribe = $rootScope.$on('meetFitApp:fitnesseventUpdate', function(event, result) {
             vm.fitnessevent = result;
         });
         $scope.$on('$destroy', unsubscribe);
+
     }
 })();
